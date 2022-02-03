0226ffa9245daa36c6f7a922364a3f100558efa0
Christian Chitanu <christian.chitanu@hotmail.com>
2022-02-03 01:19:22 -0500

    implemented animation library

--- components/sidebar/Sidebar.js
+++ components/sidebar/Sidebar.js
@@ -5,7 +5,7 @@ const Sidebar = (prop)=>{
         <>
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true}/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
         <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@100;300;500&display=swap" rel="stylesheet"/>
         </Head>
     
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import 'react-multi-carousel/lib/styles.css'
 
+import "aos/dist/aos.css";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
