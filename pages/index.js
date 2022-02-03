6ba29679154cc3be06e3b410efbb7df952a58bd4
Christian Chitanu <christian.chitanu@hotmail.com>
2022-02-02 23:08:49 -0500

    fixed and formatted a few things

--- pages/index.js
+++ pages/index.js
@@ -55,25 +55,21 @@ export default function Home() {
             <h2 className="text-black text-xl title2 ml-5">How I work</h2>
             <h2 className="text-black text-6xl title1 ml-5">Soft Skills</h2>
             {/* introduce softskill */}
-            <div className='grid sm:grid-cols-2 grid-cols-1 gap-4 mt-5'>
-              <div className='mx-auto w-[90%] text-xl'>
+            <div className='grid place-items-center sm:grid-cols-2 grid-cols-1 gap-10 mt-5'>
+              <div id="gridItem" className='mx-auto w-[90%] text-xl'>
                 <h2 className="text-black text-3xl title font-bold">Analytical Skills</h2>
                 <div>
                   <span className='font-bold'>Find ideas.</span> Since designing the logic of a website can be approached in many ways, there are many strategies that are beneficial in delivering a feature. But not all ideas are fit for the product the customer wants. It is my responsibility to analyse solutions and find what is best for the business.
                 </div>
               </div>
-              <div className='mx-auto w-[90%] text-xl'>
+              <div id="gridItem" className='mx-auto w-[90%] text-xl'>
                 <h2 className="text-black text-3xl title font-bold">Time Management</h2>
                 <div>
                   <span className='font-bold'>Plan ahead.</span> In conceptualizing project roadmaps, I plan out the development process and construct specific deadlines. Since in the end it is on the customer's best interest to have a completed product and I always aim to deliver.
                 </div>
               </div>
-              <div className='mx-auto w-[90%] text-xl'>
-                <h2 className="text-black text-3xl title font-bold">Project Planning</h2>
-                <div>Irure elit excepteur nisi ex. Ad voluptate est ex pariatur duis veniam proident laborum reprehenderit cupidatat pariatur est exercitation. Sit laborum incididunt proident ullamco.</div>
-
-              </div>
-              <div className='mx-auto w-[90%] text-xl'>
+              {/* Last item will contain the  sm:col-span-2 sm:w-[50%] */}
+              <div id="gridItem" className='mx-auto w-[90%] text-xl sm:col-span-2 sm:w-[50%]'>
                 <h2 className="text-black text-3xl title font-bold">Creative Problem Solving</h2>
                 <div>
                   <span className='font-bold'>Most beneficial solution.</span> Before finalizing a solution, I think if solutions to a problem are long term and fit the interests of the client. Solving these problems in a creative manner allows me to deliver implementations that exceeds client expectations.  
